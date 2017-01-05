7bf8258f8658185e20ddaa72b032d354f90c9392
zwzhai <1325045440@qq.com>
2017-01-06 00:44:46 +0800

    结构模式->掺和模式

--- Structural pattern/Mixin-Pattern.js
+++ Structural pattern/Mixin-Pattern.js
@@ -24,9 +24,46 @@ function extendObj(obj1, obj2){
     var obj2Key;
     for(obj2Key in obj2){
         if(obj2.hasOwnProperty(obj2Key)){
+            //在这里要注意一点。如果是引用类型，如对象，数组，那么必须深拷贝。
             obj1[obj2Key] = obj2[obj2Key];
         }
     }
     return obj1;
 }
 //定义一个单例，将log方法应用于其上
+element = {
+    allElements:[],
+    create: function(type){
+        var elem = document.createElement(type);
+        this.allElements.push(elem);
+        //如果存在就调用
+        if(typeof this.log=== "function"){
+            this.log("create an element of type:"+type);
+        }
+        return elem;
+    },
+    getAllElement: function(){
+        return this.allElements;
+    }
+}
+//定义一个类， 可以使用mixin
+function Field(type,defaultText){
+    this.type=type||"";
+    this.defaultText = defaultText||"";
+
+    if(typeof this.log==="function"){
+        this.log("create an instance of filed");
+    }
+}
+
+Field.prototype = {
+    getElement: function(){
+        var field = document.createElement("input");
+        field.setAttribute("type", this.type);
+        field.setAttribute("placeholder", this.defaultText);
+        if(typeof this.log==="function"){
+            this.log("create a DOM element with placeholder text:"+this.defaultText);
+        }
+        return field;
+    }
+}
